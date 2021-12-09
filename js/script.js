9078808781cd2be7b27bd3bbc958f698b6f9622a
ggulpodo <eulbi90@gmail.com>
2021-12-09 14:52:00 +0900

    work section 수정

--- js/script.js
+++ js/script.js
@@ -1,46 +1,3 @@
-// $(document).ready(function validation() {
-//     var name = document.getElementById("name").value;
-//     var subject = document.getElementById("subject").value;
-//     var phone = document.getElementById("phone").value;
-//     var email = document.getElementById("email").value;
-//     var message = document.getElementById("message").value;
-//     var error_message = document.getElementById("error_message").value;
-//     var text;
-
-//     error_message.style.padding = "10px";
-
-//     if(name.leght < 2) {
-//         text = "Please Enter Valid Name"
-//         error_message.innerHTML = text;
-//         return false;
-//     }
-//     if(subject.leght < 2) {
-//         text = "Please Enter Correct Subject"
-//         error_message.innerHTML = text;
-//         return false;
-//     }
-//     if(phone.leght < 2) {
-//         text = "Please Enter Valid Phone Number"
-//         error_message.innerHTML = text;
-//         return false;
-//     }
-
-//     if(email.indexOF("@") == -1 || email.leght < 6) {
-//         text = "Please Enter Valid Email"
-//         error_message.innerHTML = text;
-//         return false;
-//     }
-
-//     if(message.lengt <= 20) {
-//         text = "Please Enter More Than 20 Characters"
-//         error_message.innerHTML = text;
-//         return false;
-//     }
-//     alert("Form Submitted Succesfully!")
-//     return true;
-
-// });
-
 window.onload = function () {
 
     // swiper 슬라이드 관련
@@ -53,6 +10,7 @@ window.onload = function () {
             clickable: true,
         },
         loop: true,
+        loopSlides: 1,
         spaceBetween: 10,
         
 
@@ -72,6 +30,7 @@ window.onload = function () {
     });
     
     swiper.on('transitionEnd', function() {
+        console.log(swiper.realIndex)
         $(".slide-cont").hide().eq(swiper.realIndex).show()
     });
     
