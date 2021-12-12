891c7697ac2effee7e8e66e318eacbd1c5834fac
ggulpodo <eulbi90@gmail.com>
2021-12-12 20:09:40 +0900

    구조 수정 및 반응형 완료

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@ window.onload = function () {
     // swiper 슬라이드 관련
 
     var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
+        slidesPerView: 1,
+
+        breakpoints: {
+            // 화면의 넓이가 320px 이상일 때 
+            768: {
+                slidesPerView: 1,
+            },
+            // 화면의 넓이가 640px 이상일 때 
+            1024: {
+                slidesPerView: 2,
+            },
+            1440: {
+                slidesPerView: 3,
+            }
+        },
         centeredSlides: true,
         pagination: {
             el: ".swiper-pagination",
@@ -12,7 +26,6 @@ window.onload = function () {
         loop: true,
         loopSlides: 1,
         spaceBetween: 50,
-        
 
         // 좌우이동
         navigation: {
@@ -20,11 +33,11 @@ window.onload = function () {
             prevEl: ".swiper-button-prev",
         },
 
+
     });
-    
-    swiper.on('transitionEnd', function() {
-        console.log(swiper.realIndex)
+
+    swiper.on('transitionEnd', function () {
         $(".slide-cont").hide().eq(swiper.realIndex).show()
     });
-    
+
 }
