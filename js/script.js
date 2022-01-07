0951a5f5353e115b037de3b8a0f0f1bd9f2568b2
ggulpodo <eulbi90@gmail.com>
2022-01-07 12:49:18 +0900

    반응형 메뉴 수정

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 window.onload = function () {
 
+    
+    let muenu_icon = $('.muenu-icon');
+    let menu_icon_gnb = $('.menu-icon-gnb');
+    let black_bg = $('.black-bg');
+
+    muenu_icon.click(function () {
+        black_bg.addClass('black-bg-show');
+        menu_icon_gnb.addClass('gnb-show');
+    });
+
+    let close = $('.close');
+    close.click(function () {
+        black_bg.removeClass('black-bg-show');
+        menu_icon_gnb.removeClass('gnb-show');
+    });
+
+
     // swiper 슬라이드 관련
 
     var swiper = new Swiper(".mySwiper", {
@@ -39,5 +56,53 @@ window.onload = function () {
     swiper.on('transitionEnd', function () {
         $(".slide-cont").hide().eq(swiper.realIndex).show()
     });
+
+    const icon01 = $('.github>a:first-child');
+    const icon02 = $('.kakao>a:first-child');
+    const icon03 = $('.gmail>a:first-child');
+
+    const iconText01 = $('.github>a:last-child');
+    const iconText02 =  $('.kakao>a:last-child');
+    const iconText03 = $('.gmail>a:last-child');
+
+    iconText01.mouseenter(function() {
+        icon01.addClass('animation');
+        icon01.addClass('githubActive');
+    });
+    
+    iconText01.mouseleave(function() {
+        icon01.removeClass('animation');
+        icon01.removeClass('githubActive');
+    });
+
+    
+    iconText02.mouseenter(function() {
+        icon02.addClass('animation');
+        icon02.addClass('kakaoActive');
+    });
+    
+    iconText02.mouseleave(function() {
+        icon02.removeClass('animation');
+        icon02.removeClass('kakaoActive');
+    });
+
+    iconText03.mouseenter(function() {
+        icon03.addClass('animation');
+        icon03.addClass('gmailActive');
+    });
+    
+    iconText03.mouseleave(function() {
+        icon03.removeClass('animation');
+        icon03.removeClass('gmailActive');
+    });
+    
+
+    // iconText.mouseenter(function(){
+    //     icon.addClass('animation')
+    // });
+    
+    // iconText.mouseleave(function(){
+    //     icon.removeClass('animation')
+    // });
 
 }
